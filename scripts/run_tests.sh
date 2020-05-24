422ba1a38ef06c5ed04c0b98415882efec98fae7
Markus Waas <mail@markuswaas.de>
2020-05-24 21:43:46 +1300

    add circle ci

---
+++ scripts/run_tests.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+# Exit script as soon as a command fails.
+set -o errexit
+
+# Executes cleanup function at script exit.
+trap cleanup EXIT
+
+cleanup() {
+  # Kill the ganache instance that we started (if we started one and if it's still running).
+  if [ -n "$ganache_pid" ] && ps -p $ganache_pid > /dev/null; then
+    kill -9 $ganache_pid
+  fi
+}
+
+if [ "$SOLIDITY_COVERAGE" = true ]; then
+  ganache_port=8555
+else
+  ganache_port=8545
+fi
+
+ganache_running() {
+  nc -z localhost "$ganache_port"
+}
+
+start_ganache() {
+  npx ganache-cli --gasLimit 0x1fffffffffffff --gasPrice 0x1 --port "$ganache_port" --accounts 70 > /dev/null &
+  ganache_pid=$!
+
+  echo "Waiting for ganache to launch on port "$ganache_port"..."
+
+  while ! ganache_running; do
+    sleep 0.1 # wait for 1/10 of the second before check again
+  done
+
+  echo "Ganache launched!"
+}
+
+if ganache_running; then
+  echo "Using existing ganache instance"
+else
+  echo "Starting our own ganache instance"
+  start_ganache
+fi
+
+if [ "$SOLC_NIGHTLY" = true ]; then
+  echo "Downloading solc nightly"
+  wget -q https://raw.githubusercontent.com/ethereum/solc-bin/gh-pages/bin/soljson-nightly.js -O /tmp/soljson.js && find . -name soljson.js -exec cp /tmp/soljson.js {} \;
+fi
+
+truffle version
+
+if [ "$SOLIDITY_COVERAGE" = true ]; then
+  npx solidity-coverage
+
+  if [ "$CONTINUOUS_INTEGRATION" = true ]; then
+    cat coverage/lcov.info | npx coveralls
+  fi
+else
+  npx truffle test "$@"
+fi
